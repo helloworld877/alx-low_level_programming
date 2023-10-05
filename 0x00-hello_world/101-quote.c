e7692e1082edc328d8e83ec60f56911811b6f1f9
helloworld877 <mmghaly3@gmail.com>
2023-10-05 00:29:02 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
  * main - Entry point
  * Description: 'the program's description'
@@ -7,6 +9,6 @@
 int main(void)
 {
 const char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1,txt,sizeof(txt));
-return (0);
+syscall(SYS_write, 1,txt, sizeof(txt)); 
+return (1);
 }
