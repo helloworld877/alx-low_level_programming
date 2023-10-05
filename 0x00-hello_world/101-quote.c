d9fc5a59d27d0112e97aca960b3a526ca51c4047
helloworld877 <mmghaly3@gmail.com>
2023-10-05 00:21:03 -0700

    .

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char txt="and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+write(1,txt,sizeof(txt));
+return (0);
+}
