6e2eed2a8451e9e62899da3fefb002b1cb23c4d6
helloworld877 <mmghaly3@gmail.com>
2023-10-06 13:32:06 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-        int x;
-        int y;
+	int x;
+	int y;
 
-        for (x = 'A'; x <= 'Z'; x++)
-        {
-                int lower_x = tolower(x);
-                putchar(lower_x);
-        }
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		int lower_x = tolower(x);
+		putchar(lower_x);
+	}
 
-        for (y = 'A'; y <= 'Z'; y++)
-        {
-                putchar(y);
-        }
-        putchar('\n');
-        return (0);
+	for (y = 'A'; y <= 'Z'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
 }
