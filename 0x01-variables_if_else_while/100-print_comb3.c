cd2ed4432cb25b623073f2baec46b8c4f55c7663
helloworld877 <mmghaly3@gmail.com>
2023-10-06 14:08:43 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int tens_digit, ones_digit;
+
+	for (tens_digit = 0; tens_digit <= 9; tens_digit++)
+	{
+		for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+		{
+			putchar(tens_digit + '0');
+			putchar(ones_digit + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return 0;
+}
