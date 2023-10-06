af1be089380c5d02d5e95e48c9ddb982d1957cfa
helloworld877 <mmghaly3@gmail.com>
2023-10-06 14:03:43 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,9 +14,13 @@ int main(void)
 	for (x = 0; x < 10; x++)
 	{
 		putchar('0' + x);
-		putchar(',');
-		putchar(' ');
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
 	putchar('\n');
 	return (0);
 }
