60c581f05ba906cce336a863689276f233371fb6
helloworld877 <mmghaly3@gmail.com>
2023-10-10 20:44:31 +0300

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * add- adds two numbers.
+ *
+ * @x: A number.
+ * @y: A number.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+
+int add(int x, int y)
+{
+    int res;
+
+    res = x + y;
+    return (res);
+}
