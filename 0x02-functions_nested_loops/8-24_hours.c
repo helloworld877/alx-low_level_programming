b8577cf17bd72e3c89f50ab3902afd082dbbdc90
helloworld877 <mmghaly3@gmail.com>
2023-10-10 20:17:47 +0300

    .

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer- prints last digit.
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar(hour + '0');
+			if (hour < 10)
+			{
+				_putchar(hour + '0');
+			}
+			_putchar(':');
+
+			_putchar(minute + '0');
+			if (minute < 10)
+			{
+				_putchar(minute + '0');
+			}
+		}
+	}
+}
