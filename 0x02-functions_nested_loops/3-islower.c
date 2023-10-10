63abda946378a24687bc769747b5daa7e9488686
helloworld877 <mmghaly3@gmail.com>
2023-10-10 19:56:49 +0300

    .

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,29 +3,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
+ * _islower - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
-    if (c >= 97 && c <= 122)
-    {
-        return 1;
-    }
-    return 0;
-}
-
-int main(void)
-{
-    int r;
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (c >= 97 && c <= 122)
+	{
+		return 1;
+	}
+	return 0;
 }
