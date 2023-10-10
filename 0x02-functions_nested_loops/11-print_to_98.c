6e3c826c7ed555dcf73e42acfabaa71dbbb9ab08
helloworld877 <mmghaly3@gmail.com>
2023-10-10 20:49:46 +0300

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98- prints to 98.
+ *
+ * @n: A number.
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+
+void print_to_98(int n)
+{
+	for (int i = n; i <= 98; i++)
+	{
+		if (i == 98)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf("%d, ", i);
+		}
+	}
+}
+
+void main()
+{
+	print_to_98(12);
+}
