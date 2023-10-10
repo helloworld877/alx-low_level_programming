07be1a777f981aad0f1adda35c1d44a70b86d984
helloworld877 <mmghaly3@gmail.com>
2023-10-10 20:02:06 +0300

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,52 +3,29 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
+ * print_sign- prints sign of number.
+ *
+ * @n: A number.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
  */
 
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-}
-
-int main(void)
-{
-    int r;
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 }
