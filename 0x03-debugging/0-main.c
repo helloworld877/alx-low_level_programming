3699b0e6a827f32e0a9e20e478f93570ff51507f
helloworld877 <mmghaly3@gmail.com>
2023-10-11 11:21:48 +0300

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(0);
+
+	return (0);
+}
