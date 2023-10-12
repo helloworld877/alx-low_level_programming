1b436f1602dde88aad9a3b80ff565623e9bbd6bb
helloworld877 <mmghaly3@gmail.com>
2023-10-12 08:38:17 +0300

    .

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * mul - multiplies 2 numbers.
+ *
+ * @a: A number.
+ * @b: A number.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
