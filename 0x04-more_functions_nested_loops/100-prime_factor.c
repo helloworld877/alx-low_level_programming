74d1d23c9e27515acfbc20c082264bb8494ed222
helloworld877 <mmghaly3@gmail.com>
2023-10-12 20:25:28 +0300

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main()
+{
+	long long n;
+	long long maxPrime;
+
+	maxPrime = -1;
+	n = 25698751364526;
+
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n >>= 1;
+	}
+
+	while (n % 3 == 0)
+	{
+		maxPrime = 3;
+		n = n / 3;
+	}
+
+	for (int i = 5; i * i <= n; i += 6)
+	{
+		while (n % i == 0)
+		{
+			maxPrime = i;
+			n = n / i;
+		}
+		while (n % (i + 2) == 0)
+		{
+			maxPrime = i + 2;
+			n = n / (i + 2);
+		}
+	}
+
+	if (n > 4)
+		maxPrime = n;
+
+	printf("%lld", maxPrime);
+
+	return 0;
+}
