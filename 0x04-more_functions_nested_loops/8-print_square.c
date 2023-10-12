ec47ba557d0a5f6047944d880e9613ca298f733d
helloworld877 <mmghaly3@gmail.com>
2023-10-12 09:28:19 +0300

    .

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,18 +13,18 @@
 
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,31 +7,31 @@
 
 int main(void)
 {
-    int i, fizz, buzz;
+	int i, fizz, buzz;
 
-    for (i = 1; i <= 100; i++)
-    {
-        fizz = i % 3;
-        buzz = i % 5;
+	for (i = 1; i <= 100; i++)
+	{
+		fizz = i % 3;
+		buzz = i % 5;
 
-        if (fizz == 0 && buzz == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (fizz == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (buzz == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
+		if (fizz == 0 && buzz == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (fizz == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (buzz == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
 
-    return (0);
+	return (0);
 }
