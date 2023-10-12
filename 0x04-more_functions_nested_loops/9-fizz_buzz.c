f698be8b5a29f8a515e9849b12b4571814a2cad9
helloworld877 <mmghaly3@gmail.com>
2023-10-12 20:14:06 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,19 +16,24 @@ int main(void)
 
 		if (fizz == 0 && buzz == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (fizz == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (buzz == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+
+			printf(" ");
 		}
 	}
 	printf("\n");
