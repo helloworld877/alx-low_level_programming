bc781717df0442145b1b718f8fa41bfdfa8824a1
helloworld877 <mmghaly3@gmail.com>
2023-10-16 22:31:27 +0300

    .

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+int _strlen(char *s)
+{
+    int length;
+
+    length = 0;
+    while (*s != '\0')
+    {
+        length++;
+        s++;
+    }
+    return length;
+}
