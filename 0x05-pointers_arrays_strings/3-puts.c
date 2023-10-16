243ebf37172914abf553a7c7d7042771a000daee
helloworld877 <mmghaly3@gmail.com>
2023-10-16 22:33:48 +0300

    .

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts- Checks if a character is a letter.
+ *
+ * @str: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
