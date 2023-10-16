0881f29a8d26d994be796b47866cdb9c60f60c8c
helloworld877 <mmghaly3@gmail.com>
2023-10-16 22:31:48 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,13 +12,13 @@
  */
 int _strlen(char *s)
 {
-    int length;
+	int length;
 
-    length = 0;
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
-    return length;
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return length;
 }
