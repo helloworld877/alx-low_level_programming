d1597185f1257ff2f9d4c31ce1c1caba8af242ad
helloworld877 <mmghaly3@gmail.com>
2023-10-16 22:37:46 +0300

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+void rev_string(char *s)
+{
+	int length, i;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	s--;
+	for (i = 0; i < length; i++)
+	{
+		_putchar(*s);
+	}
+
+	_putchar('\n');
+}
