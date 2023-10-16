51e76d52fe007a37476ae47662aa674cb83b66e7
helloworld877 <mmghaly3@gmail.com>
2023-10-16 22:25:56 +0300

    .

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98- Checks if a character is a letter.
+ *
+ * @n: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
