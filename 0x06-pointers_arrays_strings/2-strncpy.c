ba327800f4a2db7cb1f8ade137e5be9e4c26fe60
helloworld877 <mmghaly3@gmail.com>
2023-10-20 08:34:19 +0300

    .

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy- Checks if a character is a letter.
+ *
+ * @dest: A pointer.
+ * @src: A pointer.
+ * @n: A number.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+		dest[j] = src[j];
+
+	return (dest);
+}
