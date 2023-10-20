779bd799440f2d8bb7e8d8352d113b121be13cb4
helloworld877 <mmghaly3@gmail.com>
2023-10-20 08:25:39 +0300

    .

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,33 +14,33 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-    for (j = 0; ((j < n) && (src[j] != '\0')); j++)
-        dest[i + j] = src[j];
-    dest[i + j] = '\0';
+	for (j = 0; ((j < n) && (src[j] != '\0')); j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
 
-    return (dest);
+	return (dest);
 }
 
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strncat(s1, s2, 1);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	printf("%s\n", ptr);
+	ptr = _strncat(s1, s2, 1024);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
 }
