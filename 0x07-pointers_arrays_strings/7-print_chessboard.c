0ff54865a7ba4669b3950005f2b92da332241d61
helloworld877 <mmghaly3@gmail.com>
2023-10-24 23:10:45 +0300

    .

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard- Checks if a character is a letter.
+ *
+ * @a: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+void print_chessboard(char (*a)[8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		printf("\n");
+	}
