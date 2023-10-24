c5e4a8c9699a8c510d600428f822d812cb4081d5
helloworld877 <mmghaly3@gmail.com>
2023-10-24 22:47:33 +0300

    .

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ * @c: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+
+	return NULL;
+}
