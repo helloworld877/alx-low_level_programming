4048fde844b7536bd2df459c28fe49593db1571f
helloworld877 <mmghaly3@gmail.com>
2023-10-24 22:58:22 +0300

    .

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strspn- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ * @accept: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	typedef enum
+	{
+		false,
+		true
+	} bool;
+
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		bool found;
+
+		found = false;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				found = true;
+				break;
+			}
+		}
+		if (!found)
+		{
+			break;
+		}
+		i++;
+		s++;
+	}
+	return i;
+}
