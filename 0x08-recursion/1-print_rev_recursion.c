4d1895316d1d439b4681eb4bb4d544f14bc745a2
helloworld877 <mmghaly3@gmail.com>
2023-10-25 19:46:29 +0300

    .

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        return;
+    }
+
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
+}
