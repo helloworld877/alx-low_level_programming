5153e5016809375447a18aa096e60a8730990e1c
helloworld877 <mmghaly3@gmail.com>
2023-10-25 19:38:46 +0300

    .

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+void _puts_recursion(char *s)
+{
+    if (s == '\0')
+    {
+        return;
+    }
+
+    _putchar(s);
+    s++;
+    _puts_recursion(s);
+}
