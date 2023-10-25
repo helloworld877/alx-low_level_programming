c9bc6cd51a80af40c3073b1ae2d052250f93e43a
helloworld877 <mmghaly3@gmail.com>
2023-10-25 20:02:27 +0300

    .

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * factorial- Checks if a character is a letter.
+ *
+ * @n: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	return n * factorial(n - 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,6 @@ typedef enum
 int _putchar(char c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
+
 #endif /* MAIN_H */
