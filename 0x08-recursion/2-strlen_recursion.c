5a6c0a18a96517cecd35537b58b4cfa709127127
helloworld877 <mmghaly3@gmail.com>
2023-10-25 19:53:07 +0300

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion- Checks if a character is a letter.
+ *
+ * @s: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*(s + 1) == '\0')
+	{
+		return (1);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
