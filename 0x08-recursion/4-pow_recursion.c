198634a8818e96ecc3b7494117037ef46e1701b8
helloworld877 <mmghaly3@gmail.com>
2023-10-25 20:05:58 +0300

    .

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion- Checks if a character is a letter.
+ *
+ * @x: A pointer.
+ * @y: A pointer.
+ *
+ * Return:
+ * True if the character is lowercase, False otherwise.
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y == 0)
+    {
+        return (1);
+    }
+    else if (y < 0)
+    {
+        return (-1);
+    }
+
+    if (y == 1)
+    {
+        return (x);
+    }
+
+    return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 
 #endif /* MAIN_H */
